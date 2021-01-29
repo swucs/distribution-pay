68e14c014872898a6d87d21706324f075048fe12
bass84 <bass84@github.com>
2021-01-29 23:51:26 +0900

    Repository 생성 및 Config 설정

---
+++ src/test/java/com/paktitucci/distribution/pay/PayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paktitucci.distribution.pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
