fc1882844efbcc35dba9e57ecfaa3daf5513d6b3
bass84 <bass84@github.com>
2021-01-31 10:44:07 +0900

    각 변수를 네이밍 컨벤션에 맞게 수정

--- src/main/java/com/paktitucci/distribution/pay/utils/token/TokenGenerator.java
+++ src/main/java/com/paktitucci/distribution/pay/utils/token/TokenGenerator.java
@@ -10,22 +10,22 @@ import static java.util.stream.Collectors.joining;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class TokenGenerator {
-    private static final char[] elements = {
+    private static final char[] ELEMENTS = {
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
             'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
             'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
             '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
     };
-    private static final int startIndexOfToken = 0;
-    private static final int endIndexOfToken = elements.length;
+    private static final int START_INDEX_OF_TOKEN = 0;
+    private static final int END_INDEX_OF_TOKEN = ELEMENTS.length;
 
     public static String generate(int tokenLength) {
         Random random = new Random(System.currentTimeMillis());
 
-        return  random.ints(startIndexOfToken, endIndexOfToken)
+        return  random.ints(START_INDEX_OF_TOKEN, END_INDEX_OF_TOKEN)
                     .limit(tokenLength)
-                    .mapToObj(index -> String.valueOf(elements[index]))
+                    .mapToObj(index -> String.valueOf(ELEMENTS[index]))
                     .collect(joining(Strings.EMPTY));
 
     }
