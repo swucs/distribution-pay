f17dc9b1a964678be91e50910bcfd0ced98b8267
bass84 <bass84@github.com>
2021-02-01 19:54:14 +0900

    JPA 설정 정보 추가

--- src/main/java/com/paktitucci/distribution/pay/config/JpaConfig.java
+++ src/main/java/com/paktitucci/distribution/pay/config/JpaConfig.java
@@ -2,8 +2,10 @@ package com.paktitucci.distribution.pay.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "com.paktitucci.distribution.pay.domain.repository")
 public class JpaConfig {
 }
