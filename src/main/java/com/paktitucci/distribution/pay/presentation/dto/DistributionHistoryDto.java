40cf62373b971102624aa085d2898ecffd30c44e
bass84 <bass84@github.com>
2021-02-01 20:21:05 +0900

    불필요한 import 제거

--- src/main/java/com/paktitucci/distribution/pay/presentation/dto/DistributionHistoryDto.java
+++ src/main/java/com/paktitucci/distribution/pay/presentation/dto/DistributionHistoryDto.java
@@ -1,10 +1,6 @@
 package com.paktitucci.distribution.pay.presentation.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.paktitucci.distribution.pay.application.dto.DistributionHistory;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
